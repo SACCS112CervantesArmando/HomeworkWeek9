41a078061427e39254db45768c52d09638f4f14e
Armando Cervantes <Familyguy350z@yahoo.com>
2015-04-19 21:14:06 -0700

    hw

---
+++ Mainevenodd.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Mainevenodd {
+
+	public static void main(String[] args)
+	{
+		
+		
+		// Constructor
+		class1evenodd welcomeMessage = new class1evenodd();
+		
+		welcomeMessage.displayMessage();
+		
+		// Constructor 2
+		class2evenodd scannerMessage = new class2evenodd();
+		
+		scannerMessage.next();
+
+		
+		
+		System.out.println("Thank you for playing. Goodbye!");
+		
+		
+	}
+}
---
+++ class1evenodd.java
@@ -0,0 +1,8 @@
+
+public class class1evenodd {
+
+	public void displayMessage()
+	{
+		System.out.println( "Welcome to the Even/Odd Game");
+	}
+}
